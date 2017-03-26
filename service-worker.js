d5174547304e1b9c04d2f5fcf7048d719f89103b
mfrazi <muhammadfahrulrazi@gmail.com>
2017-03-26 15:41:08 +0700

    first commit

---
+++ service-worker.js
@@ -0,0 +1,29 @@
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+  '/',
+  '/style.css',
+  '/bootstrap/css/bootstrap.min.css'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
